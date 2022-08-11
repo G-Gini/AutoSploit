bc38ef2f57ca390db6bca49b064900b06e789739
G-Gini <93969401+G-Gini@users.noreply.github.com>
2022-08-11 17:04:11 +0800

    Update censys.py
    support new censys api

--- api_calls/censys.py
+++ api_calls/censys.py
@@ -1,5 +1,5 @@
 import requests
-
+import traceback
 import lib.settings
 from lib.errors import AutoSploitAPIConnectionError
 from lib.settings import (
@@ -7,6 +7,7 @@ from lib.settings import (
     API_URLS,
     write_to_file
 )
+import urllib
 
 
 class CensysAPIHook(object):
@@ -31,15 +32,26 @@ class CensysAPIHook(object):
         discovered_censys_hosts = set()
         try:
             lib.settings.start_animation("searching Censys with given query '{}'".format(self.query))
-            req = requests.post(
-                API_URLS["censys"], auth=(self.id, self.token),
-                json={"query": self.query}, headers=self.user_agent,
+            req = requests.get(
+                API_URLS["censys"].format(query=self.query,cursor=""), auth=(self.id, self.token),
+                headers=self.user_agent,
                 proxies=self.proxy
             )
             json_data = req.json()
-            for item in json_data["results"]:
-                discovered_censys_hosts.add(str(item["ip"]))
+            total=1000#json_data["result"]["total"]
+            for i in range(100,total,100):
+                for item in json_data["result"]["hits"]:
+                    discovered_censys_hosts.add(str(item["ip"]))
+                next_cursor=json_data["result"]["links"]["next"]
+                req = requests.get(
+                    API_URLS["censys"].format(query= urllib.quote(self.query),cursor=urllib.quote(next_cursor)), auth=(self.id, self.token),
+                    headers=self.user_agent,
+                    proxies=self.proxy
+                )
+                json_data=req.json()
             write_to_file(discovered_censys_hosts, self.host_file, mode=self.save_mode)
             return True
         except Exception as e:
+            traceback.print_exc()
+            # print(req.text)
             raise AutoSploitAPIConnectionError(str(e))
